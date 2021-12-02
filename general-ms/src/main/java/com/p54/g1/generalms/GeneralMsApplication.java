2a8ad77521ab0b688000f249c27a811b9e4a174d
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2021-12-01 19:59:23 -0500

    Este es el primer commit con la configuración base del microservicio

---
+++ general-ms/src/main/java/com/p54/g1/generalms/GeneralMsApplication.java
@@ -0,0 +1,13 @@
+package com.p54.g1.generalms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralMsApplication.class, args);
+	}
+
+}
---
+++ general-ms/src/test/java/com/p54/g1/generalms/GeneralMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p54.g1.generalms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
